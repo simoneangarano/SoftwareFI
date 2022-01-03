a532076f87f64d9573fa2a0069a4586772698813
Fernando Fernandes <fernandofernandesantos@gmail.com>
2022-01-03 14:46:03 +0100

    Update the HansGruber injector

---
+++ hg-noise-injector/classifier_hgni_sample.py
@@ -0,0 +1,177 @@
+#!/usr/bin/python3
+
+"""Sample classifier
+This sample classifier will be used to test the Hans Gruber
+noise injector
+"""
+import sys
+
+import torch
+import torchvision
+import torchvision.transforms as transforms
+import matplotlib.pyplot as plt
+import numpy as np
+
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+
+from hans_gruber import HansGruberNI, ErrorModel
+
+DATA_PATH = "../data"
+MODEL_PATH = f"{DATA_PATH}/cifar_net.pth"
+
+
+class Net(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.conv1 = nn.Conv2d(3, 6, 5)
+        self.pool = nn.MaxPool2d(2, 2)
+        self.conv2 = nn.Conv2d(6, 16, 5)
+        self.fc1 = nn.Linear(16 * 5 * 5, 120)
+        self.fc2 = nn.Linear(120, 84)
+        self.fc3 = nn.Linear(84, 10)
+
+    def forward(self, x):
+        x = self.pool(F.relu(self.conv1(x)))
+        x = self.pool(F.relu(self.conv2(x)))
+        x = torch.flatten(x, 1)  # flatten all dimensions except batch
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
+        return x
+
+
+class NetWithNoise(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.conv1 = nn.Conv2d(3, 6, 5)
+        self.pool = nn.MaxPool2d(2, 2)
+        self.conv2 = nn.Conv2d(6, 16, 5)
+        self.noise_injector = HansGruberNI(error_model=ErrorModel.COL)
+        self.fc1 = nn.Linear(16 * 5 * 5, 120)
+        self.fc2 = nn.Linear(120, 84)
+        self.fc3 = nn.Linear(84, 10)
+
+    def forward(self, x):
+        x = self.pool(F.relu(self.conv1(x)))
+        x = self.pool(F.relu(self.conv2(x)))
+        x = self.noise_injector(x)
+        x = torch.flatten(x, 1)  # flatten all dimensions except batch
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
+        return x
+
+
+# functions to show an image
+def imshow(img):
+    img = img / 2 + 0.5  # unnormalize
+    npimg = img.numpy()
+    plt.imshow(np.transpose(npimg, (1, 2, 0)))
+    plt.show()
+
+
+def train_network():
+    transform = transforms.Compose(
+        [transforms.ToTensor(),
+         transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+
+    batch_size = 4
+
+    train_set = torchvision.datasets.CIFAR10(root='../data', train=True,
+                                             download=True, transform=transform)
+    train_loader = torch.utils.data.DataLoader(train_set, batch_size=batch_size,
+                                               shuffle=True, num_workers=2)
+    net = Net()
+    criterion = nn.CrossEntropyLoss()
+    optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
+    for epoch in range(2):  # loop over the dataset multiple times
+
+        running_loss = 0.0
+        for i, data in enumerate(train_loader, 0):
+            # get the inputs; data is a list of [inputs, labels]
+            inputs, labels = data
+
+            # zero the parameter gradients
+            optimizer.zero_grad()
+
+            # forward + backward + optimize
+            outputs = net(inputs)
+            loss = criterion(outputs, labels)
+            loss.backward()
+            optimizer.step()
+
+            # print statistics
+            running_loss += loss.item()
+            if i % 2000 == 1999:  # print every 2000 mini-batches
+                print('[%d, %5d] loss: %.3f' %
+                      (epoch + 1, i + 1, running_loss / 2000))
+                running_loss = 0.0
+
+    print('Finished Training')
+    torch.save(net.state_dict(), MODEL_PATH)
+
+
+def test_network():
+    transform = transforms.Compose(
+        [transforms.ToTensor(),
+         transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+
+    batch_size = 4
+    classes = ('plane', 'car', 'bird', 'cat',
+               'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
+    testset = torchvision.datasets.CIFAR10(root='./data', train=False,
+                                           download=True, transform=transform)
+    testloader = torch.utils.data.DataLoader(testset, batch_size=batch_size,
+                                             shuffle=False, num_workers=2)
+
+    dataiter = iter(testloader)
+    images, labels = dataiter.next()
+
+    # print images
+    imshow(torchvision.utils.make_grid(images))
+    print('GroundTruth: ', ' '.join('%5s' % classes[labels[j]] for j in range(4)))
+    net = Net()
+    net.load_state_dict(torch.load(MODEL_PATH))
+    outputs = net(images)
+    _, predicted = torch.max(outputs, 1)
+
+    print('Predicted: ', ' '.join('%5s' % classes[predicted[j]] for j in range(4)))
+
+
+def test_network_noise():
+    transform = transforms.Compose(
+        [transforms.ToTensor(),
+         transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+
+    batch_size = 4
+    classes = ('plane', 'car', 'bird', 'cat',
+               'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
+    testset = torchvision.datasets.CIFAR10(root='./data', train=False,
+                                           download=True, transform=transform)
+    testloader = torch.utils.data.DataLoader(testset, batch_size=batch_size,
+                                             shuffle=False, num_workers=2)
+
+    dataiter = iter(testloader)
+    images, labels = dataiter.next()
+
+    # print images
+    imshow(torchvision.utils.make_grid(images))
+    print('GroundTruth: ', ' '.join('%5s' % classes[labels[j]] for j in range(4)))
+    net = NetWithNoise()
+    net.load_state_dict(torch.load(MODEL_PATH))
+    outputs = net(images)
+    _, predicted = torch.max(outputs, 1)
+
+    print('Predicted: ', ' '.join('%5s' % classes[predicted[j]] for j in range(4)))
+
+
+if __name__ == '__main__':
+    option = sys.argv[1]
+    if option == "train":
+        train_network()
+    elif option == "test":
+        test_network()
+    elif option == "testni":
+        test_network_noise()
--- hg-noise-injector/hans_gruber.py
+++ hg-noise-injector/hans_gruber.py
@@ -2,53 +2,43 @@
 This file encapsulate the noise injector to be used
 in the training process
 """
+import enum
 import random
+import warnings
 
 import torch
 
 
+class ErrorModel(enum.Enum):
+    ROW, COL, BLOCK, ALL = range(4)
+
+
 class HansGruberNI(torch.nn.Module):
-    def __init__(self, input_features, output_features, error_model=None):
+    def __init__(self, error_model: ErrorModel = ErrorModel.ROW):
         super(HansGruberNI, self).__init__()
-        self.input_features = input_features
-        self.output_features = output_features
-
-        # nn.Parameter is a special kind of Tensor, that will get
-        # automatically registered as Module's parameter once it's assigned
-        # as an attribute. Parameters and buffers need to be registered, or
-        # they won't appear in .parameters() (doesn't apply to buffers), and
-        # won't be converted when e.g. .cuda() is called. You can use
-        # .register_buffer() to register buffers.
-        # nn.Parameters require gradients by default.
-        self.weight = torch.nn.Parameter(torch.empty(output_features, input_features))
-
         # Error model necessary for the forward
         self.error_model = error_model
-
-        # Not a very smart way to initialize weights
-        torch.nn.init.uniform_(self.weight, -0.1, 0.1)
 
     def forward(self, input):
         # We can inject the relative errors using only Torch built-in functions
         # Otherwise it is necessary to use AutoGrads
         output = input.clone()
-        assert len(input.shape) == 2, "Generalize this method to n-arrays"
-        rows, cols = input.shape
-        if self.error_model == "ROW":
-            relative_errors = torch.FloatTensor(1, rows).uniform_(0, 1)
-            rand_row = random.randrange(0, rows)
-            output[rand_row, :].mul_(relative_errors)
-        elif self.error_model == "COL":
-            relative_errors = torch.FloatTensor(1, cols).uniform_(0, 1)
-            rand_col = random.randrange(0, cols)
-            output[:, rand_col].mul_(relative_errors)
-        elif self.error_model == "BLOCK":
+        # TODO: Need to fix it before use
+        warnings.warn("Need to fix the HansGruber noise injector to support more than 2d dimention before use")
+        # assert len(input.shape) == 2, f"Generalize this method to n-arrays {input.shape}\n{input}"
+        # rows, cols = input.shape
+        relative_error = random.uniform(0, 100)
+
+        if self.error_model == ErrorModel.ROW:
+            # relative_errors = torch.FloatTensor(1, rows).uniform_(0, 1)
+            rand_row = random.randrange(0, input.shape[0])
+            output[rand_row, :].mul_(relative_error)
+        elif self.error_model == ErrorModel.COL:
+            # relative_errors = torch.FloatTensor(1, cols).uniform_(0, 1)
+            rand_col = random.randrange(0, input.shape[1])
+            output[:, rand_col].mul_(relative_error)
+        elif self.error_model == ErrorModel.BLOCK:
             raise NotImplementedError
-        elif self.error_model == "ALL":
+        elif self.error_model == ErrorModel.ALL:
             raise NotImplementedError
         return output
-
-    def extra_repr(self):
-        # (Optional)Set the extra information about this module. You can test
-        # it by printing an object of this class.
-        return f'input_features={self.input_features}, output_features={self.output_features}'
