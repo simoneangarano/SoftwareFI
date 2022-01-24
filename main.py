5b97293d049306b90c0eab837c849588de421343
NiccoloCavagnero <niccolo.cavagnero@studenti.polito.it>
2022-01-24 15:00:54 +0100

    Added capability of reading a .yaml configuration file which overwrites ArgParser default values.

--- main.py
+++ main.py
@@ -7,12 +7,17 @@ from pytorch_lightning.callbacks import ModelCheckpoint
 from pytorch_lightning.loggers import WandbLogger
 
 # DieHardNET packages
-from pytorch_scripts.utils import build_model, CifarDataModule
+from pytorch_scripts.utils import build_model, CifarDataModule, _parse_args
 from hg_noise_injector.hans_gruber import HansGruberNI
+
+
+config_parser = parser = argparse.ArgumentParser(description='Configuration', add_help=False)
+parser.add_argument('-c', '--config', default='', type=str, metavar='FILE',
+                    help='YAML config file specifying default arguments')
 
 parser = argparse.ArgumentParser(description='PyTorch Training')
 
-parser.add_argument('--name', default='validation_test_wefc', help='Experiment name.')
+parser.add_argument('--name', default='validation_test_wefcd', help='Experiment name.')
 parser.add_argument('--mode', default='train', help='train or validation')
 parser.add_argument('--ckpt', default=None, help='Pass the name of a checkpoint to resume training.')
 parser.add_argument('--dataset', default='cifar10', help='Dataset name: cifar10 or cifar100.')
@@ -30,18 +35,17 @@ parser.add_argument('--comment', default='ResNet trained with original settings 
 
 
 def main():
-    args = parser.parse_args()
+    args, args_text = _parse_args(parser, config_parser)
 
     # Set random seed
     pl.seed_everything(args.seed, workers=True)
 
     print('==> Loading dataset..')
-    #train_data, test_data, n_classes = get_dataset(args.dataset, args.data_dir, args.batch_size, args.num_gpus)
     cifar = CifarDataModule(args.dataset, args.data_dir, args.batch_size, args.num_gpus)
-    n_classes = 10 if args.dataset == 'cifar10' else 100
 
     # Build model (Resnet only up to now)
     optim_params = {'optimizer': args.optimizer, 'epochs': args.epochs, 'lr': args.lr, 'wd': args.wd}
+    n_classes = 10 if args.dataset == 'cifar10' else 100
     net = build_model(args.model, n_classes, optim_params)
 
     # W&B logger
@@ -55,10 +59,11 @@ def main():
     callbacks = [ckpt_callback]
 
     # Pytorch-Lightning Trainer
-    if args.ckpt:
-        args.ckpt = 'checkpoints/' + args.ckpt
     trainer = pl.Trainer(max_epochs=args.epochs, devices=args.num_gpus, callbacks=callbacks, logger=wandb_logger,
                          deterministic=True, benchmark=True, accelerator='gpu', strategy="dp", sync_batchnorm=True)
+
+    if args.ckpt:
+        args.ckpt = 'checkpoints/' + args.ckpt
     if args.mode == 'train':
         trainer.fit(net, cifar, ckpt_path=args.ckpt)
     elif args.mode == 'validation':
--- pytorch_scripts/utils.py
+++ pytorch_scripts/utils.py
@@ -1,3 +1,5 @@
+import yaml
+
 import pytorch_lightning
 from torch.utils.data import DataLoader
 from torchvision.datasets import CIFAR10, CIFAR100
@@ -70,3 +72,20 @@ def build_model(model='resnet20', n_classes=10, optim_params={}):
 
     print(f'\n    {model} built.')
     return ModelWrapper(net, n_classes, optim_params)
+
+
+def _parse_args(parser, config_parser):
+    # Do we have a config file to parse?
+    args_config, remaining = config_parser.parse_known_args()
+    if args_config.config:
+        with open(args_config.config, 'r') as f:
+            cfg = yaml.safe_load(f)
+            parser.set_defaults(**cfg)
+
+    # The main arg parser parses the rest of the args, the usual
+    # defaults will have been overridden if config file specified.
+    args = parser.parse_args(remaining)
+
+    # Cache the args as a text string to save them in the output dir later
+    args_text = yaml.safe_dump(args.__dict__, default_flow_style=False)
+    return args, args_text
