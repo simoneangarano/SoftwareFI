e20ec88cd604165e7a6959b08293ede6f06cfd0a
fernandoFernandeSantos <fernandofernandesantos@gmail.com>
2022-02-08 14:18:07 +0100

    Using the largest values for the random generation for HG.

--- hg_noise_injector/hans_gruber.py
+++ hg_noise_injector/hans_gruber.py
@@ -35,8 +35,20 @@ class HansGruberNI(torch.nn.Module):
         :return: the calculated relative_error
         """
         # TODO: Generalize the random generation to the values observed on GEMM output
-        x_min, alpha, r = 1.0728769e-07, 1.0868737e+00, random.random()
+        x_mins_fus = [1.0728769e-07, 2.0230031e+00, 8.1847715e-08, 1.3602772e+05, 3.0000000e+00, 3.5176080e-02,
+                      3.4028237e+38, 2.0000000e+00, 1.0238367e-02, 1.3968560e-09, 2.6865074e-10, 1.3970158e-09,
+                      6.6699225e-01, 6.6699225e-01, 6.6699225e-01, 7.5000001e-01, 6.1141304e-01, 7.5000001e-01,
+                      0.0000000e+00, 7.0958774e-08, 0.0000000e+00]
+        alphas_fus = [1.0868737e+00, 1.0568325e+00, 1.0820710e+00, 2.7119400e+01, 1.0678725e+00, 1.1896030e+00,
+                      4.4310700e+05, 1.4543958e+00, 1.1181921e+00, 1.0846596e+00, 1.0769672e+00, 1.0851440e+00,
+                      2.3798765e+01, 2.3798765e+01, 2.3922783e+01, 1.2143508e+08, 3.4316596e+00, 1.2143508e+08,
+                      1.0821200e+00, 1.0821160e+00, 1.0821200e+00]
+
+        alpha = max(alphas_fus)
+        x_min = x_mins_fus[alphas_fus.index(alpha)]
+        r = random.random()
         relative_error = x_min * (1 - r) ** (-1 / (alpha - 1))
+        # print(relative_error)
         return relative_error
 
     def inject(self, forward_input: torch.Tensor, p: float) -> torch.Tensor:
