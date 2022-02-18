f7a6cb4dd070fd721a1d6920a015db0aca0892f2
fernandoFernandeSantos <fernandofernandesantos@gmail.com>
2022-02-18 17:04:34 +0100

    Almost there

--- criticality_evaluation.py
+++ criticality_evaluation.py
@@ -33,22 +33,25 @@ def main() -> None:
     model_path = "data/c10_resnet20_base_adamw_2-epoch=159-val_acc=0.90.ts"
     golden_model = torch.load(model_path)
     golden_model.eval()
+    golden_model = golden_model.to("cuda")
     k = 5
-    test_loader = load_imagenet(data_dir="data", subset_size=100,
+    test_loader = load_cifar100(data_dir="data", subset_size=100,
                                 transform=torchvision.transforms.Compose([
                                     torchvision.transforms.ToTensor(),
                                     torchvision.transforms.Normalize(mean=[0.485, 0.456, 0.406],
                                                                      std=[0.229, 0.224, 0.225])]))
     with torch.no_grad():
         for i, (image, label) in enumerate(test_loader):
+            image_gpu = image.to("cuda")
             # Golden execution
             model_time = time.time()
-            gold_output = golden_model(image)
+            gold_output = golden_model(image_gpu)
             model_time = time.time() - model_time
 
-            gold_top_k_labels = torch.topk(gold_output, k=k).indices.squeeze(0)
+            gold_output_cpu = gold_output.to("cpu")
+            gold_top_k_labels = torch.topk(gold_output_cpu, k=k).indices.squeeze(0)
             gold_probabilities = torch.tensor(
-                [torch.softmax(gold_output, dim=1)[0, idx].item() for idx in gold_top_k_labels])
+                [torch.softmax(gold_output_cpu, dim=1)[0, idx].item() for idx in gold_top_k_labels])
 
             # Print gold
             print(gold_top_k_labels)
